335f95631d719c46ad13425b97a0d5b1d84d5eb9
Murilo Gonçalves <murilo.gon@live.com>
2021-11-08 16:37:33 -0300

    feat: add base routes configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        Navbar
+      </nav>
+      <Routes>
+        <Route path="/" element={(<h1>Home</h1>)} />
+        <Route path="/users-inks" element={(<h1>inks</h1>)} />
+        <Route path="/admin" element={(<h1>admin</h1>)} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </>
   );
 }
 
