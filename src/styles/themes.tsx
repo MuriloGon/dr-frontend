65feb9427cc0710269b57e93fda07d06cf3ba095
Murilo Gonçalves <murilo.gon@live.com>
2021-11-08 15:16:19 -0300

    feat: implement themes utils

--- src/styles/themes.tsx
+++ src/styles/themes.tsx
@@ -1,10 +1,17 @@
 
-const themes = {
+export type themeType = {
+  primary: string
+  primary2: string
+  secondary: string
+}
+
+const themes: { [key: string]: themeType } = {
   light: {
     primary: 'hsl(350deg 90% 60%)',
     primary2: 'hsl(350deg 90% 95%)',
     secondary: 'hsl(350deg 90% 80%)',
   }
 }
+
 
 export default themes;
