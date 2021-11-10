ed57e2ee032acb59f4f994c98d0bba2834ab6127
Murilo Gonçalves <murilo.gon@live.com>
2021-11-10 12:33:09 -0300

    feat: create api interface and mock implementation

---
+++ src/api/ApiInterface.ts
@@ -0,0 +1,30 @@
+enum CanSize {
+  '0.5L' = 0.5,
+  '2.5L' = 2.5,
+  '3.6L' = 3.6,
+  '18L' = 18,
+}
+
+export type Wall = {
+  width: number;
+  height: number;
+  doors: number;
+  windows: number;
+};
+export type Ink = {
+  '_id': string
+  'createdAt': number;
+  'canSize': CanSize
+  'wall-a': Wall;
+  'wall-b': Wall;
+  'wall-c': Wall;
+  'wall-d': Wall;
+};
+
+export default interface ApiInterface {
+  getInkById: (id: string) => Promise<{ message: string, data?: Ink }>;
+  getAllInks: () => Promise<{ message: string, data?: Ink[] }>;
+  editInkById: (editInk: Ink) => Promise<{ message: string, data?: Ink }>;
+  deleteInkById: (id: string) => Promise<{ message: string, data?: Ink }>;
+  login: (email: string, password: string) => Promise<{ message: string, token: string }>;
+}
