86f7101be25e54cca2cb5bfa7a3253f4335c9e35
Murilo Gonçalves <murilo.gon@live.com>
2021-11-10 03:38:07 -0300

    refactor: change InkCalculator to align with the InkResult component

--- src/components/InkCalculator/index.tsx
+++ src/components/InkCalculator/index.tsx
@@ -3,6 +3,7 @@ import { InkCalcContainer, InkCalcTitle } from '@styles/inkCalculator';
 import WallSelection from './WallSelection';
 import WallComputation from './WallComputation';
 import { Wall, initialState } from './_utils';
+import InkResult from './InkResult';
 
 function InkCalculator() {
   const [currentWall, setCurrentWall] = useState<Wall>(Wall.wallA1);
@@ -29,15 +30,25 @@ function InkCalculator() {
     setHeight, setWidth, setDoors, setWindows,
   };
 
+  const windowDim = { width: 2, height: 1.2 };
+  const doorDim = { width: 0.8, height: 1.9 };
+
   return (
-    <InkCalcContainer>
-      <InkCalcTitle>Ink Calculator</InkCalcTitle>
-      <WallSelection currentSelected={currentWall} onClick={setCurrentWall} />
-      <WallComputation
-        currentWall={walls[currentWall]}
-        setData={setData}
+    <>
+      <InkCalcContainer>
+        <InkCalcTitle>Ink Calculator</InkCalcTitle>
+        <WallSelection currentSelected={currentWall} onClick={setCurrentWall} />
+        <WallComputation
+          currentWall={walls[currentWall]}
+          setData={setData}
+        />
+      </InkCalcContainer>
+      <InkResult
+        windowDimension={windowDim}
+        doorDimension={doorDim}
+        walls={walls}
       />
-    </InkCalcContainer>
+    </>
   );
 }
 
