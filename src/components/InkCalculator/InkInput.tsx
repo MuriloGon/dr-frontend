51e4c0d5ca5eabf0957386342de0aeeb97a5dfe0
Murilo Gonçalves <murilo.gon@live.com>
2021-11-10 03:32:58 -0300

    refactor: add step props for InkInput compond with a default value

--- src/components/InkCalculator/InkInput.tsx
+++ src/components/InkCalculator/InkInput.tsx
@@ -4,19 +4,21 @@ import { InkInputContainer, Input } from '@styles/inkCalculator';
 type Props = {
   name: string;
   min: number;
+  step?: number;
   max: number;
   value: number;
   onChange: React.ChangeEventHandler<HTMLInputElement>
 };
 
 const InkInput: FC<Props> = ({
-  name, onChange, min, max, value,
+  name, onChange, min, max, value, step = 1,
 }) => (
   <InkInputContainer>
     <span>{name}</span>
     <Input
       min={min}
       max={max}
+      step={step}
       value={value}
       onChange={onChange}
       type="number"
